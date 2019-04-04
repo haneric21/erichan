8057e68dffc2e293578e0540fb475102016fa70d
Eric Han <eric@remine.com>
2019-04-04 14:56:28 -0400

    Updates

---
+++ precache-manifest.88e09b13becea39527baab8f0d5befdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/erichan/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7cf3a6af982ea3546ae4",
+    "url": "/erichan/static/js/runtime~main.60d5f1ae.js"
+  },
+  {
+    "revision": "71fbc1bdc54f8dab0c1f",
+    "url": "/erichan/static/js/main.88c72fb9.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/erichan/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "71fbc1bdc54f8dab0c1f",
+    "url": "/erichan/static/css/main.489af33c.chunk.css"
+  },
+  {
+    "revision": "31d96fbc27276961a698c620cc71ff73",
+    "url": "/erichan/index.html"
+  }
+];
