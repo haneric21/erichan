77edd963ea3391460251e95f7b08af5cb9d61e05
Eric Han <eric@remine.com>
2020-01-16 17:32:38 -0500

    remove Projects index.. todo projects section

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import Hero from "./Hero/Hero";
 import AboutMe from "./AboutMe/AboutMe";
 import Footer from "./Footer/Footer";
-import Projects from "./Projects/Projects";
 
-export { Hero, AboutMe, Footer, Projects };
+export { Hero, AboutMe, Footer };
